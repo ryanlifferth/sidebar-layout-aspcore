339ef8b623a4aec5d12ab99f38b78afe548b4f39
Ryan Lifferth <ryanlifferth@gmail.com>
2019-06-06 17:16:46 -0600

    Additional formatting....

---
+++ Common/CustomViewEngine.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sidebar_layout_aspcore.Common
+{
+    public class CustomViewEngine : IViewLocationExpander
+    {
+
+        private static string[] NewPartialViewFormats = new[]
+        {
+            "~/Views/{1}/PartialViews/{0}.cshtml",
+            "~/Views/Shared/PartialViews/{0}.cshtml"
+        };
+
+        public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
+        {
+            return viewLocations.Union(NewPartialViewFormats);
+        }
+
+        public void PopulateValues(ViewLocationExpanderContext context)
+        {
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -5,8 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using sidebar_layout_aspcore.Common;
 
 namespace sidebar_layout_aspcore
 {
@@ -19,11 +21,17 @@ namespace sidebar_layout_aspcore
         }
 
         public IConfiguration Configuration { get; }
+        
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<RazorViewEngineOptions>(options =>
+            {
+                options.ViewLocationExpanders.Add(new CustomViewEngine());
+            });
+
             services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_2);
         }
 
